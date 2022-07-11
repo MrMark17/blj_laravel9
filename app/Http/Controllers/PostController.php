1574e0feaa61edc6b200329593896e2cf8c4738f
MrMark17 <rizqinatamardika@students.amikom.ac.id>
2022-07-12 05:46:07 +0700

    Initial commit

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use app\models\post;
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    public function index()
+    {
+        $posts = Post::latest()->get();
+        return view('posts.index', compact('posts'));
+    }
+}
